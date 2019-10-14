56344cda36cbcd24aa16e808311d582e67be5b76
Claudiu Ciumedean <claudiuciumedean@Claudius-MacBook-Pro.local>
2019-10-14 16:02:51 +0200

    working on the app

---
+++ controllers/storeController.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (req, res) => res.render('index');
+
+exports.addStore = (req, res) => res.render('editStore', { title: 'Add Store' });
+
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+
+    req.flash('success', `Successfully creted ${store.name}`);
+    res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = (req, res) => res.render('stores', { title: 'Stores' });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { homePage, addStore, createStore, getStores } = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+router.get('/', homePage);
+router.get('/add', addStore);
+router.get('/stores', getStores);
+router.post('/add', catchErrors(createStore));
 
 module.exports = router;
